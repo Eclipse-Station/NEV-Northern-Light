63126961b7970d454b33941e0f07c2f16516bdd5
Gaz / Andreas <1784490+EvilJackCarver@users.noreply.github.com>
2020-07-11 04:36:46 -0500

    Somehow the changelog generator got merge conflicted
    I just don't even

--- tools/changelog/generate_cl.py
+++ tools/changelog/generate_cl.py
@@ -60,11 +60,8 @@ try:
     cl_list = CL_SPLIT.findall(cl.group(2))
 except AttributeError:
     print("No CL found!")
-<<<<<<< HEAD
+
     exit(0)
-=======
-    exit(0) # Change to '0' if you do not want the action to fail when no CL is provided
->>>>>>> 3e52a29... Makes changelog actions suck a bit less (#5047)
 
 
 if cl.group(1) is not None:
